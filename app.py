c2657823b302930c9951daded39788b2e1a2971e
TheOX7 <thismad07@gmail.com>
2023-12-16 12:58:39 +0700

    Add Features to Edit Weight Value

--- app.py
+++ app.py
@@ -35,20 +35,20 @@ df['Battery Removable'] = df['Battery Removable'].map(map_removability)
 
 # Define weights for each column
 weights = {
-    'Weight (Gram)': lambda x: x / 4,  # Smaller weight is better
-    'Price (£)': lambda x: x / 4,  # Smaller price is better
-    'Length (mm)': lambda x: x / 4,  # Smaller length is better
-    'Width (mm)': lambda x: x / 4,  # Smaller width is better
-    'Thickness (mm)': lambda x: x / 4,  # Smaller thickness is better
-    'Display Size Pixels': lambda x: display_size_weight(*x.split(' x ')) / 4,  # Use the display_size_weight function
-    'Internal Memory (GB)': lambda x: x / 4,  # Larger internal memory is better
-    'Primary Camera MP': lambda x: x / 4,  # Larger MP is better
-    'Secondary Camera MP': lambda x: x / 4,  # Larger MP is better
-    'Battery (mAh)': lambda x: x / 5,  # Larger mAh is better
+    'Weight (Gram)': lambda x: x * 0.25,  # Smaller weight is better
+    'Price (£)': lambda x: x * 0.25,  # Smaller price is better
+    'Length (mm)': lambda x: x * 0.25,  # Smaller length is better
+    'Width (mm)': lambda x: x * 0.25,  # Smaller width is better
+    'Thickness (mm)': lambda x: x * 0.25,  # Smaller thickness is better
+    'Internal Memory (GB)': lambda x: x * 0.25,  # Larger internal memory is better
+    'Primary Camera MP': lambda x: x * 0.25,  # Larger MP is better
+    'Secondary Camera MP': lambda x: x * 0.25,  # Larger MP is better
+    'Battery (mAh)': lambda x: x * 0.20,  # Larger mAh is better
+    'Screen to Body Ratio': lambda x: x * 0.20,  # Larger ratio is better
+    'CPU (MB)': lambda x: x * 0.20,  # Larger speed is better
+    'RAM (MB)': lambda x: x * 0.20,  # Larger RAM size is better
     'Battery Removable': lambda x: 5 if x == "Removable" else 0, 
-    'Screen to Body Ratio': lambda x: x / 5,  # Larger ratio is better
-    'CPU (MB)': lambda x: x / 5,  # Larger speed is better
-    'RAM (MB)': lambda x: x / 5,  # Larger RAM size is better
+    'Display Size Pixels': lambda x: display_size_weight(*x.split(' x ')) * 0.25,  # Use the display_size_weight function
     'Display Type': lambda x: 4 if x == 'Super AMOLED' else 3 if x == 'AMOLED' else 2 if x == 'IPS LCD' else 1 if x == 'TFT' else 0,
     'Color Support': lambda x: 3 if x == '16M' else 2 if x == '256K' else 1 if x == '65K' else 0,  # Higher weight for 16M color support
     'Additional Features': lambda x: 3 if 'Super AMOLED Plus' in x else 2 if 'Super Flexible AMOLED' in x else 1 if 'SC-LCD' in x else 0,
@@ -81,11 +81,36 @@ with col_1:
         st.text(f"{selected_model}")
     
 with col_2:
-    # Apply weights to each column
-    for col in weights:
-        if col in df.columns:
-            df_weight[col + ' Score'] = df[col].apply(weights[col])
+    with st.expander('Edit Weights') :
+        # Edit weights for each column
+        st.subheader('Edit Weights for Each Columns')
 
+        # Create a dictionary to store edited weights
+        edited_weights = {}
+
+        # Split the features into groups of 4
+        feature_groups = [list(weights.keys())[i:i+4] for i in range(0, len(weights), 4)]
+
+        for group in feature_groups:
+            # Create columns for each group
+            weight_columns = st.columns(len(group))
+
+            for col, weight_column in zip(group, weight_columns):
+                if col not in ['Battery Removable', 'Display Size Pixels', 'Display Type', 'Color Support', 'Additional Features']:
+                    # Use spinner to edit weights
+                    current_weight = weights[col](0)  # Get the current weight value by applying the lambda function to a dummy value
+                    weight_value = weight_column.number_input(f'{col} Weight', min_value=0.0, max_value=1.0, value=current_weight, step=0.01)
+                    edited_weights[col] = weight_value
+
+        # Update weights dictionary with edited values
+        for col in edited_weights:
+            weights[col] = lambda x, col=col: x * edited_weights[col]
+
+        # Apply updated weights to each column
+        for col in weights:
+            if col in df.columns:
+                df_weight[col + ' Score'] = df[col].apply(weights[col])
+                
     # Checkbox to select models
     selected_models = st.multiselect('Select models for scoring', df['Model'].unique())
 
@@ -107,6 +132,7 @@ with col_2:
 
     st.subheader('Sorted Dataset based on Overall Score (Filtered)')
     st.dataframe(sorted_filtered_df[['Brand', 'Model', 'Overall Score']])
+
 
 with col_3:
     st.subheader('CRUD Smartphone Data')
